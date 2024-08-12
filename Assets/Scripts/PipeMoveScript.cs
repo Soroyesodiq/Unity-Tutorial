a74f3f2bf4af21063403028c48406080f55122a1
Soroyesodiq <34821161+Soroyesodiq@users.noreply.github.com>
2024-08-12 09:18:37 +0100

    Update PipeMoveScript.cs
    added line of code to make the pipe move along the screen

--- Assets/Scripts/PipeMoveScript.cs
+++ Assets/Scripts/PipeMoveScript.cs
@@ -16,6 +16,7 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
         if (transform.position.x > deadZone)
         {
             Destroy(gameObject);
